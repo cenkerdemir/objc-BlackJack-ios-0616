5aaf2b658003836ca3045b433e600abc61ee8ddc
github username <github email address>
2016-06-17 15:38:57 -0400

    lab complete

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,16 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackGame.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISBlackjackGame *newGame = [[FISBlackjackGame alloc] init];
+    [newGame playBlackjack];
+    
     return YES;
 }
 
